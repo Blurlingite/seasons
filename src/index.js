a1f06db16350aef6dbe6ccf08136f09965091e8f
Vishnu Dawah <vkdawah@gmail.com>
2019-06-10 19:05:05 -0400

    Added conditional rendering for latitude, error and loading

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     // Since we expect lat to be a number eventually, we will set it to null for now
     // Now we can access the state and the data inside it in this component
     // This is the ONLY time we should directly assign the state with this.state. DO NOT do this anywhere else! You need to use setState() if you want to change the state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: "" };
 
     // this will get the user's current location (no import or install needed, it is built into most browsers)
     window.navigator.geolocation.getCurrentPosition(
@@ -24,10 +24,13 @@ class App extends React.Component {
       position => {
         // We take the latitude from "position"'s "coords" field and use that to change the state in "this.state" If you check your console you will see that latitude is in coords
         // setState comes from React.Component that we extend from and it is used to update the state
+        // B/c we are in Javascript (a synchronus language) the code in "this.setState" (the success callback function) will not run until we return from the constructor() function. It justs gets setup and passed stuff until we return, then it runs
         this.setState({ lat: position.coords.latitude });
       },
       // the failure callback (gets called we fail to find the user's current position)
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
@@ -35,7 +38,16 @@ class App extends React.Component {
   // You should not put function calls that will take some time, or initialize some work/request in the render() because render() will re-run many times (when the state is changed)
   render() {
     // We access the latitude from the state with this.state.lat
-    return <div>Latitude: {this.state.lat}</div>;
+
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
   }
 }
 
