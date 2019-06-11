eaf90e58ea01088b8e86256dcae9b9ef71d5601e
Vishnu Dawah <vkdawah@gmail.com>
2019-06-10 20:37:55 -0400

    Can now display a seasonal  message depending on user location

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+// a method used to find the season using latitude and month
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    // this is a ternary expression
+    // If the lat is greater than 0 (?), return 'summer', otherwise(:) return 'winter' (don't forget the return statement)
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    // this is just for if the month is outisde the range of 2-9
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  {
+    /* If the season is 'winter say "Burr, it is chilly", otherwise say  "Lets hit the beach" */
+  }
+  const text =
+    season === "winter" ? "Burr, it is chilly" : "Lets hit the beach";
+  return <h1>{text}</h1>;
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -3,35 +3,43 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  // special function specific to the javscript language itself, not React
-  // it is the first function called when this class is created, so it's a great place to initialze our component's state (since w need to initialize state when the component is first created)
-  // constructor is called with the props object
-  constructor(props) {
-    // when we define a constructor() function, we are overriding the constructor() function from the class we extend from (React.Component). But we still want the setup code from React.Component's constructor() function to get called so we use the "super" keyword.
-    // "super" is a reference to the parent's constructor function
-    super(props);
+  // // special function specific to the javscript language itself, not React
+  // // it is the first function called when this class is created, so it's a great place to initialze our component's state (since w need to initialize state when the component is first created)
+  // // constructor is called with the props object
+  // constructor(props) {
+  //   // when we define a constructor() function, we are overriding the constructor() function from the class we extend from (React.Component). But we still want the setup code from React.Component's constructor() function to get called so we use the "super" keyword.
+  //   // "super" is a reference to the parent's constructor function
+  //   super(props);
 
-    // initialize our state object. It will eventually contain some data
-    // We can use our state object to initialize fields that we plan to use in this component like the latitude (or lat).
-    // Since we expect lat to be a number eventually, we will set it to null for now
-    // Now we can access the state and the data inside it in this component
-    // This is the ONLY time we should directly assign the state with this.state. DO NOT do this anywhere else! You need to use setState() if you want to change the state
-    this.state = { lat: null, errorMessage: "" };
+  //   // initialize our state object. It will eventually contain some data
+  //   // We can use our state object to initialize fields that we plan to use in this component like the latitude (or lat).
+  //   // Since we expect lat to be a number eventually, we will set it to null for now
+  //   // Now we can access the state and the data inside it in this component
+  //   // This is the ONLY time we should directly assign the state with this.state. DO NOT do this anywhere else! You need to use setState() if you want to change the state
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
 
+  // this is equivalent to using a constructor() to initialize the state as seen in the commented out code above
+  // This is the alternate and more preferred way of initializing state
+  state = { lat: null, errorMessage: "" };
+
+  // it is considered good practice to put data loading code that you want initially before the component renders in componentDidMount() and NOT the constructor()
+  componentDidMount() {
     // this will get the user's current location (no import or install needed, it is built into most browsers)
     window.navigator.geolocation.getCurrentPosition(
       // The success callback function (gets called we are successful in finding the user's current position). Take the position as an argument and then console.log() the position
-      position => {
-        // We take the latitude from "position"'s "coords" field and use that to change the state in "this.state" If you check your console you will see that latitude is in coords
-        // setState comes from React.Component that we extend from and it is used to update the state
-        // B/c we are in Javascript (a synchronus language) the code in "this.setState" (the success callback function) will not run until we return from the constructor() function. It justs gets setup and passed stuff until we return, then it runs
-        this.setState({ lat: position.coords.latitude });
-      },
+      // We take the latitude from "position"'s "coords" field and use that to change the state in "this.state" If you check your console you will see that latitude is in coords
+      // setState comes from React.Component that we extend from and it is used to update the state
+      // B/c we are in Javascript (a synchronus language) the code in "this.setState" (the success callback function) will not run until we return from the constructor() function. It justs gets setup and passed stuff until we return, then it runs
+      position => this.setState({ lat: position.coords.latitude }),
+
       // the failure callback (gets called we fail to find the user's current position)
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      err => this.setState({ errorMessage: err.message })
     );
+  }
+
+  componentDidUpdate() {
+    console.log("It re-rendered");
   }
 
   // special function required by React
@@ -44,7 +52,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading...</div>;
